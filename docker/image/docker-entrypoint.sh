2373ced9dbe81fbb32f893df9eae8c00936afe60
Henrique O. Santos <hensantos@yahoo.com>
2020-02-07 16:01:07 -0500

    update entrypoint to load health care sample

--- docker/image/docker-entrypoint.sh
+++ docker/image/docker-entrypoint.sh
@@ -5,7 +5,7 @@ sleep 10
 
 # load ontologies
 cd /ontologies/
-curl -X POST -H 'Content-Type:application/rdf+xml' --data-binary '@foaf.rdf' http://blazegraph:8080/blazegraph/sparql
+curl -X POST -H 'Content-Type:application/x-turtle' --data-binary '@healthcare.ttl' http://blazegraph:8080/blazegraph/sparql
 
 # keeps the container running temporarily
 while true; do sleep 1000; done
