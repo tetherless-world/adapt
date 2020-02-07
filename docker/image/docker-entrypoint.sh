a96312718025e66f86367f578bb7640d9bd06efa
Henrique O. Santos <hensantos@yahoo.com>
2020-02-07 17:59:24 -0500

    loads policy ontology

--- docker/image/docker-entrypoint.sh
+++ docker/image/docker-entrypoint.sh
@@ -5,6 +5,7 @@ sleep 10
 
 # load ontologies
 cd /ontologies/
+curl -X POST -H 'Content-Type:application/x-turtle' --data-binary '@policy.ttl' http://blazegraph:8080/blazegraph/sparql
 curl -X POST -H 'Content-Type:application/x-turtle' --data-binary '@healthcare.ttl' http://blazegraph:8080/blazegraph/sparql
 
 # keeps the container running temporarily
