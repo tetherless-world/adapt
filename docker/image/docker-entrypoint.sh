68f820d9a75ced244acafb7364108b072f0a2860
Henrique O. Santos <hensantos@yahoo.com>
2020-02-07 15:08:58 -0500

    docker setup

---
+++ docker/image/docker-entrypoint.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+# wait for blazegraph to be up
+sleep 10
+
+# load ontologies
+cd /ontologies/
+curl -X POST -H 'Content-Type:application/rdf+xml' --data-binary '@foaf.rdf' http://blazegraph:8080/blazegraph/sparql
+
+# keeps the container running temporarily
+while true; do sleep 1000; done
