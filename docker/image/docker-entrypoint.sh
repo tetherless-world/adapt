78f0172f9a1975c97efb7e245cd728ba39c5998e
Henrique O. Santos <hensantos@yahoo.com>
2020-02-09 20:23:14 -0500

    loads sample nanopub using twks-cli

--- docker/image/docker-entrypoint.sh
+++ docker/image/docker-entrypoint.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 
 # wait for blazegraph to be up
-sleep 10
+sleep 30
 
 # load ontologies
 cd /ontologies/
-curl -X POST -H 'Content-Type:application/x-turtle' --data-binary '@policy.ttl' http://blazegraph:8080/blazegraph/sparql
-curl -X POST -H 'Content-Type:application/x-turtle' --data-binary '@healthcare.ttl' http://blazegraph:8080/blazegraph/sparql
+
+cat sample-nanopub.trig | /twks-cli -Dtwks.serverBaseUrl=http://twks-server:8080 put-nanopublications --lang trig - &
 
 # keeps the container running temporarily
 while true; do sleep 1000; done
