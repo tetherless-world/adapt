d8b20852a2556e1465ef86d596c47bc6d5b62d12
Henrique O. Santos <hensantos@yahoo.com>
2020-02-13 14:01:30 -0500

    updates ontology path

--- load-ontologies.sh
+++ load-ontologies.sh
@@ -4,8 +4,8 @@
 sleep 30
 
 # load ontologies
-cd /ontologies/
-
-cat policy.ttl | /twks-cli -Dtwks.serverBaseUrl=http://twks-server:8080 put-nanopublications --lang turtle - &
-cat healthcare.ttl | /twks-cli -Dtwks.serverBaseUrl=http://twks-server:8080 put-nanopublications --lang turtle - &
-cat healthcare-policy.ttl | /twks-cli -Dtwks.serverBaseUrl=http://twks-server:8080 put-nanopublications --lang turtle - &
+cat /ontologies/policy.ttl | /twks-cli -Dtwks.serverBaseUrl=http://twks-server:8080 put-nanopublications --lang turtle - &
+cat /ontologies/healthcare.ttl | /twks-cli -Dtwks.serverBaseUrl=http://twks-server:8080 put-nanopublications --lang turtle - &
+cat /ontologies/healthcare-policy.ttl | /twks-cli -Dtwks.serverBaseUrl=http://twks-server:8080 put-nanopublications --lang turtle - &
+cat /ontologies/dsa.ttl | /twks-cli -Dtwks.serverBaseUrl=http://twks-server:8080 put-nanopublications --lang turtle - &
+cat /ontologies/dsa-policy.ttl | /twks-cli -Dtwks.serverBaseUrl=http://twks-server:8080 put-nanopublications --lang turtle - &
