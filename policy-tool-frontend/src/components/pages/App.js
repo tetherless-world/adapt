4dbf77754d25f6efa7c5c09a8e987ad1491f8875
Mitchell Falkow <mdfalkow@gmail.com>
2020-03-02 12:46:59 -0500

    testing value selects

--- policy-tool-frontend/src/components/pages/App.js
+++ policy-tool-frontend/src/components/pages/App.js
@@ -1,10 +1,11 @@
 import React from 'react'
-import { BrowserRouter, Route, Switch } from "react-router-dom";
+import { BrowserRouter, Route, Switch } from "react-router-dom"
 import PolicyCreator from './PolicyCreator/PolicyCreator'
 import DomainSelection from './DomainSelection'
-import Layout from '../common/Layout';
+import Layout from '../common/Layout'
 import NotFound from './NotFound'
-import Home from './Home';
+import Home from './Home'
+import AttributeEditor from './Test/AttributeEditor'
 
 
 export default function App () {
@@ -14,6 +15,7 @@ export default function App () {
         <Switch >
           <Route path={'/creator'} exact component={PolicyCreator} />
           <Route path={'/domains'} exact component={DomainSelection} />
+          <Route path={'/test'} exact component={AttributeEditor} />
           <Route path={'/'} exact component={Home} />
           <Route path={'*'} component={NotFound} />
         </Switch>
