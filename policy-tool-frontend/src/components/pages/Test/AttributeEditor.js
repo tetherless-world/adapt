808a1b63fad9a2b9a4dc78eddbff74c32c67c546
Mitchell Falkow <mdfalkow@gmail.com>
2020-03-03 11:20:09 -0500

    new attribute editor

--- policy-tool-frontend/src/components/pages/Test/AttributeEditor.js
+++ policy-tool-frontend/src/components/pages/Test/AttributeEditor.js
@@ -6,10 +6,10 @@ import {
   FormControl,
   makeStyles,
   InputLabel,
-  MenuItem
+  MenuItem,
+  IconButton
 } from '@material-ui/core'
-
-import Attribute from './Attribute'
+import { Delete as DeleteIcon, Clear as ClearIcon } from '@material-ui/icons'
 
 import PreviewJson from '../../common/PreviewJson'
 import useAttributes from '../../../functions/useAttributes'
@@ -17,7 +17,20 @@ import SelectField from './components/SelectField'
 import TextInput from './components/TextInput'
 import UnknownField from './components/UnknownField'
 
+const useStyles = makeStyles(theme => ({
+  root: {},
+  attribute: {
+    paddingTop: theme.spacing(2)
+  },
+  values: {
+    paddingTop: theme.spacing(1),
+    paddingLeft: theme.spacing(4)
+  }
+}))
+
 export default function AttributeEditor() {
+  const classes = useStyles()
+
   const validAttributes = [
     {
       name: 'Start Time',
@@ -38,6 +51,14 @@ export default function AttributeEditor() {
     updateValue
   } = useAttributes()
 
+  const handleClearAll = () => {
+    setAttributes([{ name: '', type: '', values: [] }])
+  }
+
+  const handleAddAttribute = () => {
+    setAttributes(prev => [...prev, { name: '', type: '', values: [] }])
+  }
+
   const handleChangeSelectedAttribute = index => event => {
     updateAttribute(
       index,
@@ -45,18 +66,45 @@ export default function AttributeEditor() {
     )
   }
 
+  const handleAddAttributeValue = index => {
+    updateAttribute(index, {
+      ...attributes[index],
+      values: [...attributes[index].values, null]
+    })
+  }
+
   const handleChangeAttributeValue = (index, valueIndex) => value => {
     updateValue(index, valueIndex, value)
   }
 
+  const handleDeleteAttributeValue = (index, valueIndex) => {
+    updateAttribute(index, {
+      ...attributes[index],
+      values: attributes[index].values.filter((_, i) => i !== valueIndex)
+    })
+  }
+
   return (
     <>
+      <Grid container spacing={4}>
+        <Grid item>
+          <Button onClick={handleAddAttribute}>Intersect</Button>
+        </Grid>
+        <Grid item>
+          <Button onClick={handleClearAll}>Clear All</Button>
+        </Grid>
+      </Grid>
       <Grid container>
         <Grid item xs={12}>
           {attributes.map((attribute, index) => (
             <>
-              <Grid container>
-                <Grid item xs={12} sm={4}>
+              <Grid
+                container
+                spacing={2}
+                alignItems={'flex-end'}
+                className={classes.attribute}
+              >
+                <Grid item xs={12} md={4}>
                   <FormControl fullWidth>
                     <InputLabel>Attribute</InputLabel>
                     <Select
@@ -71,32 +119,56 @@ export default function AttributeEditor() {
                     </Select>
                   </FormControl>
                 </Grid>
+
+                <Grid item>
+                  <Button onClick={() => handleAddAttributeValue(index)}>
+                    Add value
+                  </Button>
+                </Grid>
               </Grid>
+
               <Grid container>
-                {attribute.values.map((value, valueIndex) => {
-                  let FieldComponent = UnknownField
+                <Grid item xs={12} md={4} className={classes.values}>
+                  {attribute.values.map((value, valueIndex) => {
+                    let FieldComponent = UnknownField
 
-                  switch (attribute.type) {
-                    case 'xsd:datetime':
-                      FieldComponent = TextInput
-                      break
-                  }
+                    switch (attribute.type) {
+                      case 'xsd:datetime':
+                        FieldComponent = TextInput
+                        break
+                    }
 
-                  return (
-                    <Grid item xs={8}>
-                      <FieldComponent
-                        key={valueIndex}
-                        value={value}
-                        field={{
-                          id: `${valueIndex}`,
-                          title: 'Value',
-                          type: attribute.type
-                        }}
-                        onChange={handleChangeAttributeValue(index, valueIndex)}
-                      />
-                    </Grid>
-                  )
-                })}
+                    return (
+                      <Grid container alignItems={'flex-end'} spacing={2}>
+                        <Grid item xs={12} md={10}>
+                          <FieldComponent
+                            key={valueIndex}
+                            value={value}
+                            field={{
+                              id: `${valueIndex}`,
+                              title: 'Value',
+                              type: attribute.type
+                            }}
+                            onChange={handleChangeAttributeValue(
+                              index,
+                              valueIndex
+                            )}
+                          />
+                        </Grid>
+                        <Grid item>
+                          <IconButton
+                            size={'small'}
+                            onClick={() =>
+                              handleDeleteAttributeValue(index, valueIndex)
+                            }
+                          >
+                            <ClearIcon />
+                          </IconButton>
+                        </Grid>
+                      </Grid>
+                    )
+                  })}
+                </Grid>
               </Grid>
             </>
           ))}
