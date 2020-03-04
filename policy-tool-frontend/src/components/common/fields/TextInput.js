69244eec1b96815a7f4653533563aeb5c790d502
Mitchell Falkow <mdfalkow@gmail.com>
2020-03-04 14:46:29 -0500

    field fix

--- policy-tool-frontend/src/components/common/fields/TextInput.js
+++ policy-tool-frontend/src/components/common/fields/TextInput.js
@@ -4,13 +4,25 @@ import { FormControl, TextField } from '@material-ui/core'
 export default function TextInputs({
   value,
   onChange,
-  field: { title },
+  field: { title, type },
   ...props
 }) {
+  let fieldType = 'text'
+  if (!!type) {
+    switch (type) {
+      case 'http://www.w3.org/2001/XMLSchema#float':
+        fieldType = 'number'
+        break
+      default:
+        fieldType = 'text'
+    }
+  }
+
   return (
     <FormControl fullWidth>
       <TextField
         label={title}
+        type={fieldType}
         value={value || ''}
         onChange={event => onChange(event.target.value)}
         fullWidth
