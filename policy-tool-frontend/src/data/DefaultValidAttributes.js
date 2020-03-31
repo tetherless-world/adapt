4e22b41ca7d7c7fc5c131deafa6e6c0c87a52c0f
Mitchell Falkow <mdfalkow@gmail.com>
2020-03-31 15:33:30 -0400

    update packages, default valid attrs, api

--- policy-tool-frontend/src/data/DefaultValidAttributes.js
+++ policy-tool-frontend/src/data/DefaultValidAttributes.js
@@ -1,21 +1,21 @@
 export const DefaultValidAttributes = [
   {
-    attr_uri: '#startTime',
+    attr_uri: 'http://www.w3.org/ns/prov#startTime',
     attr_label: 'Start time',
     value_type: 'http://www.w3.org/2001/XMLSchema#dateTime'
   },
   {
-    attr_uri: '#endTime',
+    attr_uri: 'http://www.w3.org/ns/prov#endTime',
     attr_label: 'End time',
     value_type: 'http://www.w3.org/2001/XMLSchema#dateTime'
   },
   {
-    attr_uri: '#agent',
+    attr_uri: ' http://www.w3.org/ns/prov#Agent',
     attr_label: 'Agent',
     value_type: 'http://www.w3.org/2001/XMLSchema#string'
   },
   {
-    attr_uri: '#location',
+    attr_uri: 'http://www.w3.org/ns/prov#Location',
     attr_label: 'Location',
     value_type: 'http://www.w3.org/2001/XMLSchema#string'
   }
