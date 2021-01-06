4527396da1fb47ea3dac403924ef8c2c9488ae79
Mitchell Falkow <mdfalkow@gmail.com>
2021-01-06 16:24:53 -0500

    update: FormSectionProps extends GridProps

--- policy-tool-frontend-v2/src/components/FormSection.tsx
+++ policy-tool-frontend-v2/src/components/FormSection.tsx
@@ -1,28 +1,27 @@
-import { Grid, Typography, makeStyles } from '@material-ui/core'
+import { Grid, Typography, makeStyles, GridProps } from '@material-ui/core'
 
 const useStyles = makeStyles((theme) => ({
   label: { paddingBottom: theme.spacing(1) },
 }))
 
-export interface FormSectionProps {
+export interface FormSectionProps extends GridProps {
   label: string
 }
 
 export const FormSection: React.FC<FormSectionProps> = ({
   label,
   children,
+  ...props
 }) => {
   const classes = useStyles()
   return (
-    <>
-      <Grid container>
-        <Grid item className={classes.label}>
-          <Typography variant={'h5'}>{label}</Typography>
-        </Grid>
-        <Grid item xs={12}>
-          {children}
-        </Grid>
+    <Grid container {...props}>
+      <Grid item className={classes.label}>
+        <Typography variant={'h5'}>{label}</Typography>
       </Grid>
-    </>
+      <Grid item xs={12}>
+        {children}
+      </Grid>
+    </Grid>
   )
 }
