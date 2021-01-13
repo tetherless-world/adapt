b63e5855acf9e5b7321385aad55ee39620dc2d9b
Mitchell Falkow <mdfalkow@gmail.com>
2021-01-13 15:35:33 -0500

    update: RestrictionComponent

--- policy-tool-frontend-v2/src/components/RestrictionComponent.tsx
+++ policy-tool-frontend-v2/src/components/RestrictionComponent.tsx
@@ -2,8 +2,10 @@ import { Grid, makeStyles, Typography, useTheme } from '@material-ui/core'
 import _, { PropertyPath } from 'lodash'
 import { Draft } from 'immer'
 import { useContext } from 'react'
-import { OptionMapContext } from '../../contexts/OptionMapContext'
-import { InputWrapper } from '../InputWrapper'
+import { OptionMapContext } from '../contexts/OptionMapContext'
+import { InputWrapper } from './InputWrapper'
+import { Updater } from 'use-immer'
+import { Restriction } from '../global'
 
 const useStyles = makeStyles((theme) => {
   return {
@@ -19,62 +21,59 @@ const useStyles = makeStyles((theme) => {
 
 export interface RestrictionProps {
   keys: PropertyPath
-  restrictions: [restrictions: any[], updateRestrictions: Function]
+  restrictions: Restriction[]
+  updateRestrictions: Updater<Restriction[]>
 }
 
-export const Restriction: React.FC<RestrictionProps> = (props) => {
+const updateValue = (keys: PropertyPath, value: any) => (
+  draft: Draft<Restriction[]>
+) => void _.set(draft, keys, value)
+
+export const RestrictionComponent: React.FC<RestrictionProps> = (props) => {
   let keys = _.toPath(props.keys)
-  let [restrictions, updateRestrictions] = props.restrictions
+  let { restrictions, updateRestrictions } = props
 
   const theme = useTheme()
   const classes = useStyles(theme)
   const optionMap = useContext(OptionMapContext)
 
-  const {
-    uri,
-    label,
-    values,
-    attributes,
-  }: {
-    uri: string
-    label: string
-    values?: any[]
-    attributes?: any[]
-  } = _.get(restrictions, keys)
+  let restriction: Restriction = _.get(restrictions, keys)
 
-  const options = optionMap[uri] ?? []
+  const options = optionMap[restriction.uri] ?? []
 
   return (
     <div className={classes.root}>
-      {!!attributes?.length && (
+      {!!restriction?.attributes?.length && (
         <Grid container spacing={1}>
           <Grid item xs={12}>
-            <Typography className={classes.label}>{label}</Typography>
+            <Typography className={classes.label}>
+              {restriction.label}
+            </Typography>
           </Grid>
-          {attributes.map((_, i) => (
+          {restriction.attributes.map((_, i) => (
             <Grid item xs={12}>
-              <Restriction
+              <RestrictionComponent
                 keys={[...keys, 'attributes', i]}
-                restrictions={[restrictions, updateRestrictions]}
+                restrictions={restrictions}
+                updateRestrictions={updateRestrictions}
               />
             </Grid>
           ))}
         </Grid>
       )}
-      {!!values?.length &&
-        values.map(({ value, typeUri }: { value: any; typeUri: string }, i) => (
+      {!!restriction?.values?.length &&
+        restriction.values.map((value, i) => (
           <Grid item xs={12} md={6}>
             <InputWrapper
-              typeUri={typeUri}
+              typeUri={value.type}
               options={options}
               textFieldProps={{
                 value,
                 onChange: (event: any) =>
-                  updateRestrictions((draft: Draft<any[]>) =>
-                    _.set(
-                      draft,
+                  updateRestrictions(
+                    updateValue(
                       [...keys, 'values', i, 'value'],
-                      event.target.value
+                      event?.target?.value
                     )
                   ),
               }}
