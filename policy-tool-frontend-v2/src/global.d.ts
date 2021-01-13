edb31d94cdcc688e171096326d5e60f8eca90c62
Mitchell Falkow <mdfalkow@gmail.com>
2021-01-13 15:44:23 -0500

    remove: unused globals + hooks

--- policy-tool-frontend-v2/src/global.d.ts
+++ policy-tool-frontend-v2/src/global.d.ts
@@ -1,37 +1,4 @@
-import { Draft } from 'immer'
-import { Updater } from 'use-immer'
 import { Dictionary, PropertyName, PropertyPath } from 'lodash'
-
-export interface SimpleState<S extends any> {
-  get: () => S
-  set: Updater<S>
-}
-
-export interface MapState<K, V> {
-  state: Record<K, V>
-  get: (keys: PropertyPath) => V
-  set: (keys: PropertyPath, value: V) => void
-}
-
-export interface ListState<S> extends MapState<number, S> {
-  state: S[]
-  append: (item: S) => void
-  remove: (index: number) => void
-  clear: () => void
-}
-
-export interface PolicyState {
-  data: object
-  source: SimpleState<string>
-  id: SimpleState<string>
-  label: SimpleState<string | undefined>
-  definition: SimpleState<string | undefined>
-  action: SimpleState<string | null>
-  precedence: SimpleState<string | null>
-  effects: ListState<any>
-  activityRestrictions: ListState<any>
-  agentRestrictions: ListState<any>
-}
 
 export interface Restriction {
   uri: string
