c6bf28453932ca13a82802a1fd908fc5f796228f
Mitchell Falkow <mdfalkow@gmail.com>
2021-01-15 10:23:52 -0500

    removed: unused dataclasses

--- policy-tool-backend/policy_tool_backend/models/dtos.py
+++ policy-tool-backend/policy_tool_backend/models/dtos.py
@@ -8,17 +8,3 @@ class RequestPostDTO:
         self.label: str = data['label']
         self.definition: str = data['definition']
         self.attributes: List[dict] = data['attributes']
-
-class PolicyPostDTO:
-    """Policy POST DTO Class"""
-
-    def __init__(self, data):
-        self.id: str = data['id']
-        self.label: str = data['label']
-        self.source: str = data['source']
-        self.definition: str = data['definition']
-        self.action: str = data['action']
-        self.precedence: str = data['precedence']
-        self.effects: List[dict] = data['effects']
-        self.obligations: List[dict] = data['obligations']
-        self.attributes: List[dict] = data['attributes']
