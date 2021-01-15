87042a1637c032e58107e49343ae62a0f4c6dc90
Mitchell Falkow <mdfalkow@gmail.com>
2021-01-15 12:24:31 -0500

    update: app renders pages

--- policy-tool-frontend-v2/src/App.tsx
+++ policy-tool-frontend-v2/src/App.tsx
@@ -1,9 +1,21 @@
-function App() {
-  return (
-    <div className="App">
-      App
-    </div>
-  );
-}
+import { MuiThemeProvider } from '@material-ui/core'
+import { Route, BrowserRouter as Router, Switch } from 'react-router-dom'
+import { Layout } from './components'
+import { Builder, Home, NotFound } from './pages'
+import { theme } from './theme'
 
-export default App;
+export const App: React.FC = () => (
+  <div>
+    <MuiThemeProvider theme={theme}>
+      <Router>
+        <Layout title={'Policy Builder'}>
+          <Switch>
+            <Route path={'/builder'} component={Builder} />
+            <Route path={'/'} exact component={Home} />
+            <Route path={'*'} component={NotFound} />
+          </Switch>
+        </Layout>
+      </Router>
+    </MuiThemeProvider>
+  </div>
+)
