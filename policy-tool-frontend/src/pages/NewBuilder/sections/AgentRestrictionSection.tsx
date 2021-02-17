49457c97b28f8b7b5ff3f611547328a2184e14bc
Mitchell Falkow <mdfalkow@gmail.com>
2021-02-17 13:05:52 -0500

    update: task separation in AgentRestrictionSection

--- policy-tool-frontend/src/pages/NewBuilder/sections/AgentRestrictionSection.tsx
+++ policy-tool-frontend/src/pages/NewBuilder/sections/AgentRestrictionSection.tsx
@@ -4,12 +4,17 @@ import { createSelector } from '@reduxjs/toolkit'
 import _, { Dictionary } from 'lodash'
 import { useContext } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
-import { actions } from 'src/store'
 import { MenuButton } from 'src/components'
 import { RestrictionComponent } from 'src/components/NewRestrictionComponent/RestrictionComponent'
 import { LabelByURIContext } from 'src/contexts'
-import { AgentRestriction, NewPolicyState } from 'src/global'
+import { useLabels } from 'src/hooks/useLabels'
+import { actions } from 'src/store'
 import { restrictionsSelector } from 'src/store/policy'
+import {
+  AgentRestriction,
+  isAgentRestriction,
+  isRestrictionNode,
+} from 'src/types/restrictions'
 
 export interface AgentRestrictionSectionProps {
   validRestrictions: Dictionary<AgentRestriction>
@@ -18,12 +23,19 @@ export interface AgentRestrictionSectionProps {
 const agentRestrictionsSelector = createSelector(
   restrictionsSelector,
   (restrictions) => {
-    let [a, ...rest] = restrictions
-    let i = _.findLastIndex(
-      rest,
-      (r) => r['owl:onProperty'] === 'prov:wasAssociatedWith'
+    let iMin = restrictions.findIndex(
+      (r) => isRestrictionNode(r) && isAgentRestriction(r)
     )
-    return restrictions.slice(1, i + 1) as AgentRestriction[]
+    let iMax = restrictions.findIndex(
+      (r) => isRestrictionNode(r) && !isAgentRestriction(r)
+    )
+    if (iMin === -1) return []
+    if (iMax === -1) {
+      let len = restrictions.length
+      return restrictions.slice(1, len - 1) as AgentRestriction[]
+    } else {
+      return restrictions.slice(iMin, iMax - iMin) as AgentRestriction[]
+    }
   }
 )
 
@@ -31,26 +43,37 @@ export const AgentRestrictionSection: React.FC<AgentRestrictionSectionProps> = (
   validRestrictions,
 }) => {
   const dispatch = useDispatch()
+
   const labelByURI = useContext(LabelByURIContext)
   const restrictions = useSelector(agentRestrictionsSelector)
+
   const restrictionURIs: string[] = Object.keys(validRestrictions)
-  const restrictionLabels = restrictionURIs.map((r) => ({
-    label: labelByURI[r],
-  }))
+
+  const restrictionLabels = useLabels(labelByURI, restrictionURIs)
+
+  const handleSelectOption = (i: number) => {
+    let uri = restrictionURIs[i]
+    let restriction = _.cloneDeep(validRestrictions[uri])
+    let action = actions.addAgentRestriction(restriction)
+    dispatch(action)
+  }
+
+  const handleResetAgentRestrictions = () => {
+    dispatch(actions.resetAgentRestrictions())
+  }
+
+  const handleDeleteAgentRestriction = (i: number) => () => {
+    dispatch(actions.deleteAgentRestriction(i))
+  }
 
   return (
     <>
       <Grid container item spacing={2}>
         <Grid item xs={12} md={4}>
           <MenuButton
+            // TODO: redo this component, it's not right
             options={restrictionLabels}
-            onSelectOption={(i) =>
-              dispatch(
-                actions.addAgentRestriction(
-                  validRestrictions[restrictionURIs[i]]
-                )
-              )
-            }
+            onSelectOption={handleSelectOption}
             buttonProps={{
               children: 'Add',
               disabled: !validRestrictions.length,
@@ -58,9 +81,7 @@ export const AgentRestrictionSection: React.FC<AgentRestrictionSectionProps> = (
           />
         </Grid>
         <Grid item xs={12} md={4}>
-          <Button onClick={() => dispatch(actions.resetAgentRestrictions)}>
-            Reset
-          </Button>
+          <Button onClick={handleResetAgentRestrictions}>Reset</Button>
         </Grid>
         <Grid container item spacing={2}>
           {restrictions.map((r, i) => (
@@ -73,7 +94,7 @@ export const AgentRestrictionSection: React.FC<AgentRestrictionSectionProps> = (
               key={i}
             >
               <Grid item>
-                <IconButton onClick={() => actions.deleteAgentRestriction(i)}>
+                <IconButton onClick={handleDeleteAgentRestriction(i)}>
                   <Delete />
                 </IconButton>
               </Grid>
