b8095859236a0d5793420234ae22c107f3db3118
Mitchell Falkow <mdfalkow@gmail.com>
2021-02-19 13:02:30 -0500

    update: usePostPolicy

--- policy-tool-frontend/src/hooks/api/usePostPolicy.ts
+++ policy-tool-frontend/src/hooks/api/usePostPolicy.ts
@@ -1,10 +1,11 @@
 import { useAsyncFn } from 'react-use'
+import { PolicyState } from 'src/types/policy'
 import { axios } from './common'
 
-const postPolicy = (policy: any) => async () => {
+const postPolicy = (policy: PolicyState) => async () => {
   let { data } = await axios.post('/policies', policy)
   return data
 }
 
-export const usePostPolicy = (policy: any) =>
+export const usePostPolicy = (policy: PolicyState) =>
   useAsyncFn<() => Promise<string>>(postPolicy(policy), [policy])
