eff8504c32fbe0d673acd8b871a9e99e078d044e
Mitchell Falkow <mdfalkow@gmail.com>
2021-02-28 16:09:26 -0500

    update: functioning policy graph view
    - update: using cytoscape
    
    - update: use dagre (cytoscape-dagre) layout

--- policy-tool-frontend/src/pages/View/View.tsx
+++ policy-tool-frontend/src/pages/View/View.tsx
@@ -1,79 +1,108 @@
-import { Grid, Paper, TextField, Typography } from '@material-ui/core'
-import { useEffect, useMemo } from 'react'
-import { Graph as GraphComponent } from 'react-d3-graph'
-import { useHistory } from 'react-router-dom'
+import {
+  Grid,
+  makeStyles,
+  Paper,
+  TextField,
+  Typography,
+} from '@material-ui/core'
+import cytoscape from 'cytoscape'
+import dagre from 'cytoscape-dagre'
+import { useEffect, useMemo, useState } from 'react'
+import { Redirect, useHistory } from 'react-router-dom'
 import { useLocation } from 'react-use'
 import { LoadingWrapper } from 'src/components'
-import { useGetPolicyVisualization } from 'src/hooks/api'
+import { useGetPolicy } from 'src/hooks/api'
+import { convertToGraph } from './helpers'
+
+cytoscape.use(dagre)
 
 export interface ViewProps {}
 
 const useQuery = () => new URLSearchParams(useLocation().search)
 
+const useStyles = makeStyles((theme) => ({
+  graphComponent: {
+    height: 720,
+    width: '100%',
+  },
+}))
+
 export const View: React.FC<ViewProps> = () => {
   const query = useQuery()
   const history = useHistory()
   const uri = query.get('uri')
+  const classes = useStyles()
 
   if (!uri) {
     history.push('/404')
   }
 
-  const [policyRes, getPolicy] = useGetPolicyVisualization(uri ?? '')
+  const { loading, error, value } = useGetPolicy(uri ?? '')
 
-  useEffect(() => void getPolicy(), [getPolicy])
+  const { policy, labelByURI } = value ?? {}
 
-  const policy = useMemo(() => policyRes.value ?? { nodes: [], links: [] }, [
-    policyRes,
-  ])
+  const [graphData, root] = useMemo(
+    () =>
+      !!policy && !!labelByURI
+        ? convertToGraph(policy, labelByURI)
+        : [{ nodes: [], edges: [] }, { data: { id: '' } }],
+    [policy, labelByURI]
+  )
 
-  return (
-    <LoadingWrapper loading={policyRes?.loading}>
+  const [cyEl, setCyEl] = useState<HTMLElement | null>(null)
+
+  useEffect(() => setCyEl(document.getElementById('cy')))
+
+  const cy =
+    !!cyEl &&
+    cytoscape({
+      container: cyEl,
+      elements: graphData,
+      wheelSensitivity: 0.2,
+      layout: {
+        // Dagre does not have ts declarations, so it causes errors.
+        // @ts-expect-error
+        name: 'dagre',
+        spacingFactor: 1.75,
+        nodeDimensionIncludesLayout: true,
+        nodeSep: 100,
+      },
+      style: [
+        {
+          selector: 'node',
+          style: { label: 'data(label)' },
+        },
+        {
+          selector: 'edge',
+          style: { label: 'data(label)' },
+        },
+      ],
+    })
+
+  return error ? (
+    <Redirect to={'/404'} />
+  ) : (
+    <LoadingWrapper loading={loading}>
       <Grid container>
-        <Grid container item xs={12} justify={'space-between'}>
-          <Grid item xs={4}>
-            <Grid container spacing={3}>
-              <Grid item xs={12}>
-                <Typography variant={'h5'}>View Policy</Typography>
-              </Grid>
-              <Grid item xs={12}>
-                <TextField
-                  label={'Identifier'}
-                  value={uri}
-                  disabled
-                  margin={'dense'}
-                />
-              </Grid>
-            </Grid>
+        <Grid container item>
+          <Typography variant={'h4'}>View</Typography>
+        </Grid>
+        <Grid container item>
+          <Grid item xs={12} md={6}>
+            <TextField
+              label={'Identifier'}
+              value={uri}
+              margin={'dense'}
+              disabled
+            />
           </Grid>
-          <Grid item xs={8}>
-            <Grid container item xs={12} justify={'center'}>
-              <Paper variant={'outlined'}>
-                <Grid container item xs={12} justify={'center'}>
-                  <GraphComponent
-                    data={policy}
-                    id={'graph-view'}
-                    config={{
-                      directed: true,
-                      nodeHighlightBehavior: true,
-                      height: 720,
-                      width: 720,
-                      node: {
-                        labelProperty: (n: any) => n.label,
-                        color: 'lightgreen',
-                        size: 200,
-                        highlightStrokeColor: 'blue',
-                      },
-                      link: {
-                        highlightColor: 'lightblue',
-                        renderLabel: false,
-                      },
-                    }}
-                  />
-                </Grid>
-              </Paper>
-            </Grid>
-          </Grid>
+        </Grid>
+        <Grid container item xs={12} justify={'center'}>
+          <Paper
+            variant={'outlined'}
+            id={'cy'}
+            className={classes.graphComponent}
+          />
         </Grid>
       </Grid>
     </LoadingWrapper>
